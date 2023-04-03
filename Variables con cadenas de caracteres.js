80d5571f1296a811736871d9147d36411e08bdf7
lu-castro <luciacastro.23.5@gmail.com>
2023-04-03 16:49:04 -0300

    variables con string y barra invertida dentro de este

---
+++ Variables con cadenas de caracteres.js
@@ -0,0 +1,11 @@
+var nombre = "Alan"; 
+/* la variable string se escribe dentro de comillas simples o 
+complejas siendo consistentes en su uso, utilizando la misma al principio y al final */ 
+
+
+var barraInvertida = "Soy una cadena de caracteres \"con comillas  \""; 
+console.log(barraInvertida);
+/* Esta cadena de caracteres va a imprimir comillas dentro de las barras 
+invertidas del string */
+
+
